663db5e016a33d7a39eb2520081257a6f8dd1e49
KamariRoss <kamarimobile@gmail.com>
2020-03-24 21:22:06 -0400

    explorer mode finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
-const App = () => {
-  return <HelloWorld />
-}
+class App extends React.Component {
+  state = {
+    inches: 0,
+    feet: 0,
+  }
+  converter = (e) => {
+    const inches = e.target.value
+    const feet = inches / 12
+    this.setState({ feet: feet, inches: inches })
+  }
 
+  render() {
+    return (
+      <main>
+        <h1>
+          There are {this.state.inches} inches in {this.state.feet} feet
+        </h1>
+        <input
+          id="input"
+          type="number"
+          name="input"
+          onChange={this.converter}
+        ></input>
+        {/* <button type="button">Convert</button> */}
+      </main>
+    )
+  }
+}
 export default App
